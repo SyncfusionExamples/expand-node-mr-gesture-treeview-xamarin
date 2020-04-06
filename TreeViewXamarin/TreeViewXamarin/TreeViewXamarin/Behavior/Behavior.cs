6da4502b5ad1b084476a99a3a2f13e1cdcc13bc8
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-06 17:34:03 +0530

    XAMARIN-33993 - Sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Behavior/Behavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MR.Gestures;
+using Syncfusion.TreeView.Engine;
+using Syncfusion.XForms.TreeView;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Behavior : Behavior<MR.Gestures.Grid>
+    {
+        #region Overrrides
+        protected override void OnAttachedTo(MR.Gestures.Grid bindable)
+        {
+            bindable.Tapped += Bindable_Tapped;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(MR.Gestures.Grid bindable)
+        {
+            bindable.Tapped -= Bindable_Tapped;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Event
+
+        private void Bindable_Tapped(object sender, TapEventArgs e)
+        {
+            var treeViewNode = ((sender as MR.Gestures.Grid).BindingContext) as TreeViewNode;
+            var treeView = (sender as MR.Gestures.Grid).Parent as SfTreeView;
+            treeView.ExpandNode(treeViewNode);
+        }
+        #endregion
+    }
+}
